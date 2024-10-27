a0dce5ea941d16b9e3515319880a765783ec4272
Kabir Shah <iamkbrsh@gmail.com>
2024-10-27 15:11:26 -0700

    v2 working version

--- v2.py
+++ v2.py
@@ -1,10 +1,8 @@
 import carla
 import numpy as np
 import lib.frenet_optimal_trajectory_planner.FrenetOptimalTrajectory.fot_wrapper as fot
-from agent.controller import VehiclePIDController
+from v2_controller import VehiclePIDController
 
-HOST = '127.0.0.1'
-PORT = 2000
 FOT_HYPERPARAMETERS = {
     "max_speed": 25.0,
     "max_accel": 15.0,
@@ -28,82 +26,118 @@ FOT_HYPERPARAMETERS = {
     "klon": 1.0,
     "num_threads": 0,
 }
+REPLAN_THRESHOLD = 5
+TARGET_SPEED = 5
+STOP_CONTROL = carla.VehicleControl(brake=1.0)
+
+# TODO: get data from actual GNSS sensor instead of getting
+# perfect vehicle data from CARLA
+class CarlaGnssSensor():
+    def __init__(self, actor):
+        self.actor = actor
+
+    def get_location(self):
+        return self.actor.get_location()
+
+    def get_velocity(self):
+        return self.actor.get_velocity()
+
+class CarlaCar():
+    def __init__(self, world, blueprint, spawn_point, destination, debug=False):
+        self.world = world
+        self.actor = world.spawn_actor(blueprint, spawn_point)
+        self.gnss_sensor = CarlaGnssSensor(self.actor)
+        self.car = Car(self.actor.get_location(), self.actor.get_velocity(), destination, self.gnss_sensor)
+
+        self.debug = debug
+        self.debug_last_trajectory = None
+        if debug:
+            self.debug_init(spawn_point, destination)
+
+    def run_step(self):
+        # spectator_rotation = self.actor.get_transform().rotation
+        # spectator_rotation.pitch -= 20
+        # spectator_transform = carla.Transform(self.actor.get_transform().transform(carla.Location(x=-10,z=5)), spectator_rotation)
+        # self.world.get_spectator().set_transform(spectator_transform)
+
+        self.actor.apply_control(self.car.run_step())
+        if self.debug:
+            self.debug_step()
+
+    def debug_init(self, spawn_point, destination):
+        self.world.debug.draw_string(spawn_point.location, 'start', draw_shadow=False, color=carla.Color(r=255, g=0, b=0), life_time=120.0, persistent_lines=True)
+        self.world.debug.draw_string(destination, 'end', draw_shadow=False, color=carla.Color(r=255, g=0, b=0), life_time=120.0, persistent_lines=True)
+
+    def debug_step(self):
+        trajectory = self.car.trajectory
+        if trajectory == self.debug_last_trajectory: return
+        self.debug_last_trajectory = trajectory
+        for loc in trajectory:
+            self.world.debug.draw_string(loc, 'o', draw_shadow=False, color=carla.Color(r=255, g=0, b=0), life_time=1.0, persistent_lines=True)
+
+    def destroy(self):
+        self.actor.destroy()
 
 class Car():
-    def __init__(self, world, blueprint, spawn_point, destination):
-        self.actor = world.spawn_actor(blueprint, spawn_point)
-        self.pos = self.actor.get_location()
-        self.vel = self.actor.get_velocity()
+    def __init__(self, pos, vel, destination, gnss_sensor):
+        self.pos = pos
+        self.vel = vel
         self.ps = 0
         self.obs = []
         self.destination = destination
-        self.controller = VehiclePIDController(self.actor, {'K_P': 1.95, 'K_I': 0.05, 'K_D': 0.2, 'dt': 0.05}, {'K_P': 1.0, 'K_I': 0.05, 'K_D': 0, 'dt': 0.05})
+        self.controller = VehiclePIDController({'K_P': 1.95, 'K_I': 0.05, 'K_D': 0.2, 'dt': 0.05}, {'K_P': 1.0, 'K_I': 0.05, 'K_D': 0, 'dt': 0.05})
+        self.gnss_sensor = gnss_sensor
+        self.trajectory = []
         # TODO: mount sensors
 
     def perceive(self):
-        self.pos = self.actor.get_location()
-        self.vel = self.actor.get_velocity()
+        self.pos = self.gnss_sensor.get_location()
+        self.vel = self.gnss_sensor.get_velocity()
         # TODO: get/update obstacles from sensor data only
 
     def plan(self):
-        # TODO: only replan trajectory if necessary
-        ps = self.ps
+        # if we're at destination, stop
+        # TODO: also stop if unexpected obstacle detected
         pos = self.pos
         destination = self.destination
-        obs = self.obs
-        initial_conditions = {
-            'ps': ps,
-            'target_speed': 20,
-            'pos': np.array([pos.x, pos.y]),
-            'vel': np.array([0, 0]),
-            'wp': np.array([[pos.x, pos.y], [destination.x, destination.y]]),
-            'obs': np.array(obs)
-        }
-        result_x, result_y, speeds, ix, iy, iyaw, d, s, speeds_x, speeds_y \
-            misc, costs, success = fot.run_fot(initial_conditions, FOT_HYPERPARAMETERS)
+        if pos.distance(destination) < 1.0:
+            return STOP_CONTROL
 
-        trajectory = []
-        for x, y in zip(result_x, result_y):
-            trajectory.append(carla.Location(x=x, y=y, z=pos.z))
+        # remove visited points from trajectory
+        trajectory = self.trajectory
+        num_to_remove = 0
 
-        # TODO: stop if reached destination OR if unexpected obstacle detected by sensor
-        self.actor.apply_control(self.controller.run_step(5, trajectory[1]))
+        for loc in trajectory:
+            if pos.distance(loc) < 1.0:
+                num_to_remove += 1
+            else:
+                break
+
+        trajectory = self.trajectory = trajectory[num_to_remove:]
+
+        # replan trajectory if needed
+        if len(trajectory) < REPLAN_THRESHOLD:
+            ps = self.ps
+            obs = self.obs
+            initial_conditions = {
+                'ps': ps,
+                'target_speed': TARGET_SPEED,
+                'pos': np.array([pos.x, pos.y]),
+                'vel': np.array([0, 0]),
+                'wp': np.array([[pos.x, pos.y], [destination.x, destination.y]]),
+                'obs': np.array(obs)
+            }
+            result_x, result_y, speeds, ix, iy, iyaw, d, s, speeds_x, speeds_y, \
+                misc, costs, success = fot.run_fot(initial_conditions, FOT_HYPERPARAMETERS)
+
+            trajectory = []
+            for x, y in zip(result_x, result_y):
+                trajectory.append(carla.Location(x=x, y=y, z=pos.z))
+            self.trajectory = trajectory
+
+        if not trajectory: return STOP_CONTROL
+        return self.controller.run_step(self.vel, TARGET_SPEED, self.pos, trajectory[1])
 
     def run_step(self):
         self.perceive()
-        self.plan()
-
-    def destroy():
-        self.actor.destroy()
-
-def init_simulation():
-    # connect to client
-    client = carla.Client(HOST, PORT)
-    client.set_timeout(2.0)
-
-    # setup world map
-    world = client.load_world('Town04_Opt')
-    world.unload_map_layer(carla.MapLayer.ParkedVehicles)
-
-    # TODO: setup spectator
-
-    return world
-
-def main():
-    print(f"starting simulation on {HOST}:{PORT}")
-    world = init_simulation()
-    blueprint = world.get_blueprint_library().filter('vehicle.mercedes.coupe_2020')[0]
-    spawn_point = world.get_map().get_spawn_points()[0]
-    car = Car(world, blueprint, spawn_point)
-
-    try:
-        while True:
-            world.wait_for_tick()
-            car.run_step()
-    except KeyboardInterrupt:
-        print("stopping simulation")
-    finally:
-        car.destroy()
-
-if __name__ == '__main__':
-    main()
+        return self.plan()
---
+++ v2_experiment.py
@@ -0,0 +1,83 @@
+import carla
+import random
+from parking_position import player_location_Town04, parking_vehicle_locations_Town04, parking_vehicle_rotation
+from v2 import CarlaCar
+
+HOST = '127.0.0.1'
+PORT = 2000
+DEBUG = True
+EGO_VEHICLE = 'vehicle.tesla.model3'
+PARKED_VEHICLES = [
+    'vehicle.mercedes.coupe_2020',
+    'vehicle.dodge.charger_2020',
+    'vehicle.ford.mustang',
+    'vehicle.jeep.wrangler_rubicon',
+    'vehicle.lincoln.mkz_2017'
+]
+
+def main():
+    try:
+        print(f"starting simulation on {HOST}:{PORT}")
+        client = carla.Client(HOST, PORT)
+        client.set_timeout(10.0)
+
+        # load map
+        world = client.load_world('Town04_Opt')
+        world.unload_map_layer(carla.MapLayer.ParkedVehicles)
+
+        # load spectator
+        spectator_location = carla.Location(x=285.9, y=-210.73, z=40)
+        spectator_rotation = carla.Rotation(pitch=-90.0)
+        world.get_spectator().set_transform(carla.Transform(spectator_location, spectator_rotation))
+
+        # load car
+        destination_parking_spot = 25
+        destination_parking_spot_loc = parking_vehicle_locations_Town04[destination_parking_spot]
+        destination_parking_spot_loc.x += 2
+        blueprint = world.get_blueprint_library().filter(EGO_VEHICLE)[0]
+        car = CarlaCar(world, blueprint, player_location_Town04, destination_parking_spot_loc, debug=DEBUG)
+
+        # load parked cars
+        blueprints = world.get_blueprint_library().filter('vehicle.*.*')
+        blueprints = [bp for bp in blueprints if bp.id in PARKED_VEHICLES]
+        spawn_points = [24, 26]
+        parked_cars = []
+        parked_cars_bbs = []
+
+        for i in spawn_points:
+            spawn_point = parking_vehicle_locations_Town04[i]
+
+            npc_transform = carla.Transform(spawn_point, rotation=random.choice(parking_vehicle_rotation))
+            npc_bp = random.choice(blueprints)
+            npc = world.spawn_actor(npc_bp, npc_transform)
+            npc.set_simulate_physics(False)
+            parked_cars.append(npc)
+            parked_cars_bbs.append([
+                spawn_point.x - npc.bounding_box.extent.x, spawn_point.y - npc.bounding_box.extent.y,
+                spawn_point.x + npc.bounding_box.extent.x, spawn_point.y + npc.bounding_box.extent.y
+            ])
+
+        for i, loc in enumerate(parking_vehicle_locations_Town04):
+            if i == destination_parking_spot or i in spawn_points: continue
+            parked_cars_bbs.append([
+                loc.x - 2.4, loc.y - 0.96,
+                loc.x + 2.4, loc.y + 0.96
+            ])
+
+        # HACK: enable perfect perception of parked cars
+        car.car.obs = parked_cars_bbs
+
+        # run simulation
+        while True:
+            world.wait_for_tick()
+            car.run_step()
+
+    except KeyboardInterrupt:
+        print("stopping simulation")
+    finally:
+        car.destroy()
+        for parked_car in parked_cars:
+            parked_car.destroy()
+
+if __name__ == '__main__':
+    main()
