4bd78559a07f5f990d08f3120996b5bb5aa5448a
Kabir Shah <iamkbrsh@gmail.com>
2024-12-03 22:02:28 -0800

    stick to 10 frame pause because 15 segfaults

--- v2.py
+++ v2.py
@@ -235,8 +235,8 @@ class CarlaCar():
             )
             recording_file.write_frame(data, pixel_format='bgr24')
             if self.car.mode == Mode.PARKED:
-                for _ in range(15):
-                    recording_file.write_frame(data, pixel_format='bgr24')
+                for _ in range(10):
+                    recording_file.write_frame(data.copy(), pixel_format='bgr24')
         cam.listen(on_image)
         return cam
     
